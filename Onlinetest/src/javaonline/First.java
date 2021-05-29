900c48811ca2b6a9cdc43acf456333b27884f37c
venkataramanareddy6 <reddyvenkataramana1996@gmail.com>
2021-05-29 07:52:31 +0530

    adding onlinetest

---
+++ Onlinetest/src/javaonline/First.java
@@ -0,0 +1,15 @@
+package javaonline;
+
+public class First {
+
+	//public class MathUtils {
+	    public static int average(int a, int b) {
+	        return  a + b /2 ;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(average(2,1));
+	    }
+	}
+
+
